8ee61681dcd363909ef367eece0113d24832c47a
David Vuckovic <david.vuckovic@gmx.at>
2025-11-24 04:19:01 +0100

    Email Login & Logout (zur hälfte fertig)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
-import 'Pages/Login_Page.dart';
+import 'package:cruise_connect/Pages/Auth_Page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -11,10 +18,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Cruise Connect',
-      home: LoginPage(),
+      home: AuthPage(),
     );
   }
 }
